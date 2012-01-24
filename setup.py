8e0ba0b3cd62ba7bbc7db81bf5eaeed912f9b3fc
Ryan McGowan <ryan@ryanmcg.com>
2012-01-23 19:49:20 -0500

    Updated setup.py to have some more information.

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from mr_repo import version
 
 setup(name='Mr-Repo',
       version=version,
+      author='Ryan McGowan',
+      author_email='ryan@ryanmcg.com',
+      description='A very simple repo manager of repos.',
+      url='http://packages.python.org/Mr-Repo',
       py_modules=['mr_repo'],
+      requires=['PyYAML']
       )
