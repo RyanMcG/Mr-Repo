95f7038e8459bff387397f4bec910ce050a53844
Ryan McGowan <ryan@ryanmcg.com>
2012-01-31 16:41:21 -0500

    Updated tests.

--- tests/test_mr_repo.py
+++ tests/test_mr_repo.py
@@ -3,7 +3,28 @@ from mr_repo import MrRepo
 import os
 import git
 import tempfile
-import shutil
+import copy
+
+
+def clone_state(mr_repo):
+    """Clones the state of a mr_repo instance."""
+    if isinstance(mr_repo, MrRepo):
+        return {'config': copy.deepcopy(world.mr_repo.config),
+                'repos': world.mr_repo.repos[:]}
+    else:
+        return None
+
+
+@step
+def I_have_a_mr_repo_repository(clean=True):
+    args = ['init']
+    if clean:
+        args.append('--clean')
+    world.mr_repo = MrRepo(args=args, execute=True)
+    world.before = clone_state(world.mr_repo)
+    if clean:
+        world.assertDictEqual(world.before['config'], {'repos': {}})
+        world.assertListEqual(world.before['repos'], [])
 
 
 @step
@@ -26,35 +47,37 @@ def I_setup_files():
 @step
 def I_execute_the_command():
     result = world.mr_repo.execute()
-    assert isinstance(result, str)
+    world.assertIsInstance(result, str)
 
 
 @step
 def I_have_mr_repo_config_files():
     assert os.path.isfile(world.mr_repo.config_path)
-    assert world.mr_repo.config_file.writeable()
-    assert None != world.mr_repo.config.get('repos')
+    world.assertIsNotNone(world.mr_repo.config.get('repos'))
     assert os.path.isfile(world.mr_repo.repo_file_path)
-    assert world.mr_repo.repo_file_path.writeable()
-    assert isinstance(world.mr_repo.repos.get('repos'), list)
+    world.assertIsInstance(world.mr_repo.repos, list)
 
 
 @step
 def there_is_a_git_repository_called(repo_name, bare=False):
-    tdir = tempfile.mkdtemp()
+    tdir = tempfile.mkdtemp(prefix="mrrepo")
     repo_dir = os.path.join(tdir, repo_name)
     world.git_repo = git.Repo.init(repo_dir, bare=bare)
 
 
 @step
 def I_add_the_repository(repo_path):
+    world.mr_repo.args.command = 'add'
     world.mr_repo.args.path = repo_path
     world.mr_repo.add_command()
 
 
 @step
-def mr_repo_updates_config_files():
-    pass
+def mr_repo_updates_config_files(config_check=world.assertNotEqual,
+        repo_check=world.assertNotEqual):
+    world.mr_repo.read_config()
+    config_check(world.mr_repo.config, world.before['config'])
+    repo_check(world.mr_repo.config, world.before['repos'])
 
 
 class MrRepoStories(TestCase):
@@ -65,6 +88,13 @@ class MrRepoStories(TestCase):
         super(MrRepoStories, self).setUp()
         world.mr_repo = MrRepo()
 
+    def tearDown(self):
+        super(MrRepoStories, self).tearDown()
+        world.mr_repo = None
+        #if hasattr(world, 'git_repo'):
+            #if isinstance(world.git_repo, git.Repo):
+                #shutil.rmtree(world.git_repo.working_dir)
+
     def test_running_init_creates_files(self):
         """When init is run, the config files are created and they are
         normal."""
@@ -74,13 +104,9 @@ class MrRepoStories(TestCase):
         And.I_execute_the_command()
         Then.I_have_mr_repo_config_files()
 
-    def test_adding_a_git_repo():
+    def test_adding_a_git_repo(self):
+        """Test that adding a valid directory works."""
         Given.there_is_a_git_repository_called("shoes")
+        And.I_have_a_mr_repo_repository()
         When.I_add_the_repository(world.git_repo.working_dir)
         Then.mr_repo_updates_config_files()
-
-    def tearDown(self):
-        super(MrRepoStories, self).tearDown()
-        world.mr_repo = None
-        if isinstance(world.git_repo, git.Repo):
-            shutil.rmtree(world.git_repo.working_dir)
